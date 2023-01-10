b2ede0476b569786087d3cceab428955f4b4e5ad
Jungwon <101785318+togodot@users.noreply.github.com>
2023-01-10 13:22:33 +0900

    Update Weather Observation Station 20.sql

--- SQL(HackerRank)/Weather Observation Station 20.sql
+++ SQL(HackerRank)/Weather Observation Station 20.sql
@@ -1,8 +1,10 @@
 -- < Weather Observation Station 20 >
--- https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
 
-with temp as (
-select LAT_N, row_number() over(order by LAT_N) as rn from STATION)
+with temp as
+(
+  select LAT_N, row_number() over(order by LAT_N) as rn from STATION
+)
 select round(LAT_N, 4)
 from temp
 where rn = (select count(LAT_N) from STATION) / 2;
