c0d53251f1f3b315c73075e2b911437d7ce83d7f
Jungwon <101785318+togodot@users.noreply.github.com>
2023-01-21 18:20:53 +0900

    Update 183. Customers Who Never Order.sql

--- SQL(LeetCode)/183. Customers Who Never Order.sql
+++ SQL(LeetCode)/183. Customers Who Never Order.sql
@@ -1,4 +1,6 @@
 -- < 183. Customers Who Never Order >
 -- 문제 링크: https://leetcode.com/problems/customers-who-never-order/?envType=study-plan&id=sql-i
 
+-- mysql
 select name as Customers from Customers a left join Orders b on a.id=b.customerId where b.id is null;
+
