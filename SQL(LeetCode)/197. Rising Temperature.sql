bd479a08a77b0b471a396daa9de608482c0931f9
Jungwon <101785318+togodot@users.noreply.github.com>
2023-01-22 17:02:33 +0900

    Update 197. Rising Temperature.sql

--- SQL(LeetCode)/197. Rising Temperature.sql
+++ SQL(LeetCode)/197. Rising Temperature.sql
@@ -1,7 +1,7 @@
 -- < 197. Rising Temperature >
 -- 문제 링크: https://leetcode.com/problems/rising-temperature/description/
 
--- mysql
+-- mysql (datediff 함수 사용)
 select a.id
 from Weather a join Weather b
 where a.temperature > b.temperature and datediff(a.recordDate, b.recordDate) = 1;
