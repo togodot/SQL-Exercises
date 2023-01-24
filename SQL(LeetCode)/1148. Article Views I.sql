ffae3fa9b610885d8e9638e20ededb5c637b3747
Jungwon <101785318+togodot@users.noreply.github.com>
2023-01-24 21:27:13 +0900

    Create 1148. Article Views I.sql

---
+++ SQL(LeetCode)/1148. Article Views I.sql
@@ -0,0 +1,8 @@
+-- < 1148. Article Views I >
+-- 문제 링크: https://leetcode.com/problems/article-views-i/?envType=study-plan&id=sql-i
+
+-- mysql
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by 1;
