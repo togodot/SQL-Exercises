04e6eb02dc093f1e6e6a6ddd3e03eef80e9426ac
Jungwon <101785318+togodot@users.noreply.github.com>
2023-02-03 19:20:10 +0900

    Update 627. Swap Salary.sql

--- SQL(LeetCode)/627. Swap Salary.sql
+++ SQL(LeetCode)/627. Swap Salary.sql
@@ -1,6 +1,7 @@
 -- < 627. Swap Salary >
 -- 문제 링크: https://leetcode.com/problems/swap-salary/submissions/881227055/?envType=study-plan&id=sql-i
 
+-- mysql
 update Salary
 set sex = case when sex = 'f' then 'm'
                when sex = 'm' then 'f' end;
