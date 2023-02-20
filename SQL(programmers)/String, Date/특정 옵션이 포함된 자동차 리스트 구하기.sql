0a64383969b5f4b1ee6250b20ea40b803c6ebb88
Jungwon <101785318+togodot@users.noreply.github.com>
2023-02-20 22:17:22 +0900

    Update 특정 옵션이 포함된 자동차 리스트 구하기.sql

--- SQL(programmers)/String, Date/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ SQL(programmers)/String, Date/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -1,6 +1,7 @@
 -- < 특정 옵션이 포함된 자동차 리스트 구하기 >
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/157343
 
+-- mysql
 SELECT *
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE OPTIONS LIKE '%네비게이션%'
