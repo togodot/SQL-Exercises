fc8db392920533834025d4695a5e0a7ed64df001
Jungwon <101785318+togodot@users.noreply.github.com>
2024-08-14 20:58:05 +0900

    Create  조건에 맞는 아이템들의 가격의 총합 구하기.sql

---
+++ SQL(programmers)/SUM, MAX, MIN/조건에 맞는 아이템들의 가격의 총합 구하기.sql
@@ -0,0 +1,7 @@
+-- < 조건에 맞는 아이템들의 가격의 총합 구하기 >
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273709
+
+-- mysql
+SELECT SUM(PRICE) AS TOTAL_PRICE
+FROM ITEM_INFO
+WHERE RARITY = 'LEGEND';
